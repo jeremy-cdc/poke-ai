8688940f9a72feccfe691094e97c829a109756c0
Jeremy <jeremy664k@gmail.com>
2026-02-07 22:03:34 -0300

    Feat user interface (add first feat pokedex, search pokemon, exit and management errors)

--- pokeai.py
+++ pokeai.py
@@ -5,6 +5,17 @@ os.system("clear")
 
 pokeapi_url = "https://pokeapi.co/api/v2/pokemon"
 
+def clear_console():
+    os.system("clear")
+
+    print("██████╗  ██████╗ ██╗  ██╗███████╗ █████╗ ██╗")
+    print("██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝██╔══██╗██║")
+    print("██████╔╝██║   ██║█████╔╝ █████╗  ███████║██║")
+    print("██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██╔══██║██║")
+    print("██║     ╚██████╔╝██║  ██╗███████╗██║  ██║██║")
+    print("╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝")
+    print("PokéAPI + OpenAI\n\n")
+
 def get_pokemon_types(types_data):
     pokemon_types = []
 
@@ -56,7 +67,7 @@ def get_pokemon_locations(locations_url):
         return pokemon_locations
 
     except requests.RequestException as e:
-        print(f"Ocurrió un error en la petición al buscar la locación del pokémon: {e}")
+        print(f"\nOcurrió un error en la petición al buscar la locación del pokémon: {e}")
 
 def get_pokemon_games(games_data):
     pokemon_games = []
@@ -87,8 +98,8 @@ def get_pokemon(name):
 
         pokemon_contract = {
             "name": name.capitalize(),
-            "weight": pokemon_data["weight"],
-            "height": pokemon_data["height"],
+            "weight": pokemon_data["weight"] / 10,
+            "height": pokemon_data["height"] / 10,
             "exp": pokemon_data["base_experience"],
             "abilities": get_pokemon_abilities(pokemon_data["abilities"]),
             "games": get_pokemon_games(pokemon_data["game_indices"]),
@@ -101,22 +112,140 @@ def get_pokemon(name):
         return pokemon_contract
     
     except requests.RequestException as e:
-        print(f"Ocurrió un error en la petición de buscar un pokémon: {e}")
+        print(f"\nOcurrió un error en la petición de buscar un pokémon: {e}")
 
 def get_pokemon_list(limit):
     try:
+        pokemon_list = []
+
         pokeapi_requests = requests.get(f"{pokeapi_url}?limit={limit}")
         pokemon_data_list = pokeapi_requests.json()
 
-        pokemon_list = pokemon_data_list["results"]
+        pokemon_list_iterable = pokemon_data_list["results"]
 
-        for pokemon_data in pokemon_list:
+        for pokemon_data in pokemon_list_iterable:
             name = pokemon_data["name"]
             pokemon = get_pokemon(name)
 
-            print("\n", pokemon)
+            pokemon_list.append(pokemon)
+
+        return pokemon_list
     
     except requests.RequestException as e:
-        print(f"Ocurrió un error en la petición a la lista de pokémons: {e}")
+        print(f"\nOcurrió un error en la petición a la lista de pokémons: {e}")
+        return []
 
-get_pokemon_list(5)  
+def print_pokemon(pokemon):
+    if len(pokemon) == 0:
+        return []
+    
+    width = len(pokemon["name"]) + 8  # margen a los lados
+
+    print("\n" + "=" * width)
+    print(pokemon["name"].center(width))
+    print("=" * width)
+    
+    print("\n🔮 Pokémon tipo:")
+    for types in pokemon["types"]:
+        print(f"• {types}")
+
+    print(f"\n⭐ Experiencia base: {pokemon["exp"]}")
+    print(f"⚖️  Peso: {pokemon["weight"]} kg")
+    print(f"📏 Altura: {pokemon["height"]} m")
+    
+    print("\n🧠 Habilidades")
+    for ability in pokemon["abilities"]:
+        print(f"• {ability}")
+
+    print("\n📊 Estadísticas")
+    for stats in pokemon["stats"][:5]:
+        for stat, value in stats.items():
+            print(f"• {stat}: {value}")
+
+    print("\n💥 Movimientos")
+    print("• ", end="")
+    for move in pokemon["moves"][:10]:
+        print(f"{move}", end=" • ")
+
+    print("\n\n🎮 Aparece en los siguientes juegos")
+    print("• ", end="")
+    for game in pokemon["games"][:10]:
+        print(f"{game}", end=" • ")
+
+    print("\n\n🗺️  Ubicaciones")
+    for location in pokemon["locations"][:5]:
+        print(f"• {location}")
+
+def print_pokemon_list():
+    clear_console()
+    
+    while True:
+        try:
+            print("¿Cuántos pokémones quieres mostrar?")
+            limit_list = int(input("\n▶ "))
+            pokemon_list = get_pokemon_list(limit_list)
+
+            for pokemon in pokemon_list:
+                print_pokemon(pokemon)
+            
+            ### Aquí iría el código para avanzar de páginas en la pokedex
+            print("\n")
+            break
+
+        except ValueError:
+            print("\n⚠ La cantidad de pokémones a mostrar no es válida. Vuelve a intentarlo ⚠\n")
+
+def search_pokemon():
+    clear_console()
+
+    while True:
+        try:
+            print("¿Qué pokémon deseas buscar?")
+            name_of_pokemon = input("\n▶ ")
+
+            pokemon = get_pokemon(name_of_pokemon)
+            print_pokemon(pokemon)
+
+            ### Aquí iría código sobre buscar otro pokémon o retirarse
+            print("\n")
+            break
+
+        except Exception: 
+                print("⚠ El nombre del pokémon no es válido. ¿Deseas volver a intentar (s/n)? ⚠")
+                user_confirm = input("\n▶ ").lower().strip()
+
+                print("\n")
+                if user_confirm == "n" or user_confirm == "no":
+                    break
+
+def pokedex():
+    print("██████╗  ██████╗ ██╗  ██╗███████╗ █████╗ ██╗")
+    print("██╔══██╗██╔═══██╗██║ ██╔╝██╔════╝██╔══██╗██║")
+    print("██████╔╝██║   ██║█████╔╝ █████╗  ███████║██║")
+    print("██╔═══╝ ██║   ██║██╔═██╗ ██╔══╝  ██╔══██║██║")
+    print("██║     ╚██████╔╝██║  ██╗███████╗██║  ██║██║")
+    print("╚═╝      ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝╚═╝")
+    print("PokéAPI + OpenAI\n\n")
+
+    while True:
+        print("▓▓▓  POKEAI Menú  ▓▓▓")
+        print("[1] VER POKEDEX")
+        print("[2] BUSCAR POKÉMON")
+        print("[3] SALIR")
+        
+        choice = input("\n▶ ")
+
+        if choice not in ["1", "2", "3"]:
+            clear_console()
+            print("⚠ Debes escoger una opción válida. Vuelve a intentarlo ⚠\n")
+            continue
+
+        if choice == "1":
+            print_pokemon_list()
+        elif choice == "2": 
+            search_pokemon()
+        elif choice == "3":
+            print("\n¡Hasta luego, entrenador!")
+            break
+
+pokedex()
